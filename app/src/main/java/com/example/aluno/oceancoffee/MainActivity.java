b66736a466a3f10ef762ac2015cba5347ce9f33e
Jeferson Barros <im.jbalves@gmail.com>
2016-09-30 00:51:50 -0300

    Add strings.xml and submitOrder method

--- app/src/main/java/com/example/aluno/oceancoffee/MainActivity.java
+++ app/src/main/java/com/example/aluno/oceancoffee/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.aluno.oceancoffee;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,27 +16,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitOreder(){
+    public void submitOrder(View view){
 
+        int price = calculatePrice(quantity,5);
+        String summary = createOrderSummary(quantity,price);
+
+        TextView summaryView = (TextView) findViewById(R.id.summaryOrder);
+
+        summaryView.setText(summary);
     }
 
     public void displayPrice(){
 
     }
-    public void increment(){
+
+    public void increment(View view){
 
     }
-    public void decrement(){
+
+    public void decrement(View view){
 
     }
+
     public void displayQuantity(){
 
     }
-    public void createOrderSummary(){
 
+    private String createOrderSummary(int quantity, int price){
+        String message = getString(R.string.nome);
+        message += getString(R.string.quantidade) + quantity;
+        message += getString(R.string.total) + price;
+        message += getString(R.string.thanks);
+        return message;
     }
-    public int calculatePrice(int quantity, int priceByCup){
-        return quantity*priceByCup;
 
+    private int calculatePrice(int quantity, int priceByCup){
+        return quantity*priceByCup;
     }
 }
