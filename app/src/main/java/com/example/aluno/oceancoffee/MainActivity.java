3f4e244cb220eb61dcfbc89d01531fc7a5c55fed
Jeferson Barros <im.jbalves@gmail.com>
2016-10-06 19:59:29 -0300

    Alter design

--- app/src/main/java/com/example/aluno/oceancoffee/MainActivity.java
+++ app/src/main/java/com/example/aluno/oceancoffee/MainActivity.java
@@ -27,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.good_layout);
+        //setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
         //Enable transitions
         //getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
